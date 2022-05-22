834e5d754141cebc33b16c891172f76be4eaad01
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-05-22 17:21:06 +0530

    #include<stdio.h> int main() {     int num;     scanf("%d",&num);     int i,j,sum=0,c=0;     for (i=1; i<=num; i++)     {         sum=(i*i);         if (sum==num)         {             c=1;             break;         }     }     if (c==1)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Square_Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int i,j,sum=0,c=0;
+    for (i=1; i<=num; i++)
+    {
+        sum=(i*i);
+        if (sum==num)
+        {
+            c=1;
+            break;
+        }
+    }
+    if (c==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
