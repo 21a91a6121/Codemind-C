0f46cd54c454f769ac765f9633eaa2793eceef0b
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-10 20:33:57 +0530

    #include<stdio.h> #include<math.h> int main () {     int m,n,i,j,count=0,c=0;     scanf("%d%d",&m,&n);     if(m==1)     m=2;     for(j=m;j<=n;j++)     {         count=0;         for(i=2;i<=sqrt(j);i++)         {             if(j%i==0)             count++;         }         if(count==0)         c++;     }     printf("%d",c);     return 0; }

--- Primes_in_range.c
+++ Primes_in_range.c
@@ -1,35 +1,22 @@
 #include<stdio.h>
 #include<math.h>
-int main() {
-	int i,n,flag,c;
-	int l,r;
-	
-
-	scanf("%d",&l);
-
-	scanf("%d",&r);
-	
-	c = 0; 
-
-	for(n = l;n<=r;++n){
-		
-
-		if(n==1)
-		continue;
-		flag = 0;
-		for(i = 2;i<=sqrt(n);++i){
-			
-			if(n%i==0){
-				flag=1;
-				break;
-			}
-		}
-		
-		if(flag==0)
-			++c;
-			
-	}
-	
-	printf("%d",c);
-	
+int main ()
+{
+    int m,n,i,j,count=0,c=0;
+    scanf("%d%d",&m,&n);
+    if(m==1)
+    m=2;
+    for(j=m;j<=n;j++)
+    {
+        count=0;
+        for(i=2;i<=sqrt(j);i++)
+        {
+            if(j%i==0)
+            count++;
+        }
+        if(count==0)
+        c++;
+    }
+    printf("%d",c);
+    return 0;
 }
