ca6908ee51204c0054f5c91ce91f8059bd39e66b
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-30 21:14:37 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int  max=arr[0];     for(i=0;i<n;i++)     {         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max); }

--- maximum_of_an_array.c
+++ maximum_of_an_array.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int arr[100],n,i,max;
+    int n;
     scanf("%d",&n);
+    int arr[n],i;
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    max = arr[0];
+    int  max=arr[0];
     for(i=0;i<n;i++)
     {
-        if(max<arr[i])
+        if(arr[i]>max)
         {
-            max = arr[i];
+            max=arr[i];
         }
     }
     printf("%d",max);
-    return 0;
 }
