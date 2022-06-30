d7c5997dda84410fb7c9cd55192f19ddf0ce093d
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-30 21:02:01 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c,s,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         s=0;         if(i%2!=0)         {             c++;             if(arr[i]%2!=0)             {                 s++;             }         }         else if(i%2==0)         {             k=0;             if(arr[i]%2!=0)             {                 k++;             }         }
        }
        if(c==s && k==0)
        {
            printf("True");
        }
        else if(k>0)
        {
            printf("False");
        }
    }

--- Strictly_ODD.c
+++ Strictly_ODD.c
@@ -1,27 +1,41 @@
+
 #include<stdio.h>
 int main()
 {
-    int n,i,count=0;
+    int n,arr[100],i,c,s,k;
     scanf("%d",&n);
-    int arr[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
     for(i=0;i<n;i++)
-    {
-        if(arr[i]%2!=0)
+    { 
+        c=0;
+        s=0;
+        if(i%2!=0)
         {
-            if(i%2==0)
+            c++;
+            if(arr[i]%2!=0)
             {
-                count++;
-                break;
+                s++;
             }
         }
+        else if(i%2==0)
+        {
+            k=0;
+            if(arr[i]%2!=0)
+            {
+                k++;
+            }
+        }
+        
     }
-    if(count==0)
-    printf("True");
-    else
-    printf("False");
-    return 0;
+    if(c==s && k==0)
+    {
+        printf("True");
+    }
+    else if(k>0)
+    {
+        printf("False");
+    }
 }
