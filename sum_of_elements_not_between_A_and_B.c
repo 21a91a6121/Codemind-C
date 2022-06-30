b391e4d8ebaf060fdd8767abf27e8d85ad83980f
21a91a6121 <98638809+21a91a6121@users.noreply.github.com>
2022-06-30 20:41:56 +0530

    #include<stdio.h> int main() {     int i,n,a,b,arr[100],s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>a && arr[i]>b || arr[i]<a && arr[i]<b)         {             s=s+arr[i];         }     }     {         printf("%d",s);     } }

--- sum_of_elements_not_between_A_and_B.c
+++ sum_of_elements_not_between_A_and_B.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int i,n,a,b,arr[100],s=0;
     scanf("%d",&n);
-    int arr[n],i,sum=0;
     for(i=0;i<n;i++)
     {
-       scanf("%d",&arr[i]);
+        scanf("%d",&arr[i]);
     }
-     int a,b;
     scanf("%d%d",&a,&b);
     for(i=0;i<n;i++)
     {
-        if(arr[i]<a || arr[i]>b)
+        if(arr[i]>a && arr[i]>b || arr[i]<a && arr[i]<b)
         {
-            sum+=arr[i];
+            s=s+arr[i];
         }
     }
-   printf("%d",sum);
-    
+    {
+        printf("%d",s);
+    }
 }
